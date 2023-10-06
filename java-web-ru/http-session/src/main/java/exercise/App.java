8cab3aa7a54f091e3dc6f08d892c84ac740fd622
@hexlet/cli <support@hexlet.io>
2023-10-06 22:16:53 +0300

    submit java-web-ru/http-session

--- java-web-ru/http-session/src/main/java/exercise/App.java
+++ java-web-ru/http-session/src/main/java/exercise/App.java
@@ -15,7 +15,7 @@ public final class App {
         });
 
         // BEGIN
-        app.get("/users", ctx->{
+        app.get("/users", ctx-> {
             var page = ctx.queryParamAsClass("page", Integer.class).getOrDefault(1);
             var per = ctx.queryParamAsClass("per", Integer.class).getOrDefault(5);
             var offset = (page - 1) * per;
